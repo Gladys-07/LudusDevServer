8d24bbde0c55804e2e9b22da048b1b2cbfde42de
DanielaHG <A00838702@tec.mx>
2025-04-30 19:39:26 -0600

    added newRecord in DB route

--- controllers/record.controllers.js
+++ controllers/record.controllers.js
@@ -1,6 +1,103 @@
+import e from "express";
 import { pool } from "../DB/DB.js"
 
-export const getRecord = (req, res) => res.send("Hola desde API de LudusDev");
+export const newRecord = (req, res) => {
+    try{
+        const { estadoTiempo, estacion, tipoRegistro } = req.body; 
+            if (tipoRegistro !== null){
+                pool.execute("insert into registro( estadoTiempo, estacion, tipoRegistro) values (?, ?, ?)", 
+                    [estadoTiempo, estacion, tipoRegistro], 
+                    (error, results) => {
+                        if (error){
+                            res.status(500).json({
+                                msg: error.message, 
+                            });
+                            return;
+                        } 
+                        // console.log(results)
+                        // res.status(200).json({
+                        //     msg: "Base registry done, but particular not completed",
+                        // })
+                        switch(tipoRegistro){
+                            case "Fauna en Transecto":
+                                {numeroTransecto, tipoAnimal, nombreComun, nombreCientifico }
+                                pool.execute("insert into ")
+                                break;
+                            case "Fauna en Punto de Conteo":
+                                break;
+                            case "Fauna Búsqueda Libre":
+                                break;
+                            case "Validación de Cobertura":
+                                break;
+                            case "Parcela de Vegetación":
+                                break;
+                            case "Camaras Trampa":
+                                newCamaraTrampa(req);
+                                break;
+                            case "Variables Climáticas":
+                                break;
+                            default:
+                                console.log("Done");
+                        }
+                    }
+                )
+            }
+    } catch(error){
+        console.log(error.message);
+    }
+};
+
+const newFauna = () => {
+    return;
+}
+
+const newCamaraTrampa = (req) => {
+    const { codigo, zona, nombreCamara, placa, placaGuaya, 
+        anchoCaminoMt, fechaInstalacion, distanciaObjetivoMt, 
+        alturaLenteMt, listaChequeo } = req.body;
+
+    pool.execute(`insert into camaras_trampa(codigo, zona, 
+        nombreCamara, placaCamara, placaGuaya, anchoCaminoMt, 
+        fechaInstalacion, distanciaObjetivoMt, alturaLenteMt, 
+        listaChequeo)  values(?,?,?,?,?,?,?,?,?,?)`),
+        [codigo, zona, nombreCamara, placa, placaGuaya, anchoCaminoMt, 
+        fechaInstalacion, distanciaObjetivoMt, alturaLenteMt, listaChequeo],
+        (error, results) => {
+            if (error){
+                res.status(500).json({msg : error.message});
+                return;
+            }
+            console.log("Inserted new data into ")
+            res.status(200).json({
+                msg: "New camara trampa",
+                results: results
+            })
+        }
+    newEviObser(req);
+    return;
+}
+
+const newEviObser = (req) => {
+    const {observaciones, evidencias} = req.body;
+    pool.execute("insert into evidencias(evidencias, observaciones) values(?, ?)", 
+       [evidencias, observaciones], 
+       (error, results) => {
+        if (error){
+            res.status(500).json({
+                msg: error.message
+            });
+            return;
+        } 
+        console.log("New observation registered");
+        res.status(200).json({
+            msg: "New evidence",
+            results: results
+        })
+       } 
+    )
+    return;
+}
+
 export const getRecords = (req, res) => {
     pool.execute("select * from customer", (error, results) => {
         if (error){
--- routes/record.routes.js
+++ routes/record.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getRecords, getRecord } from "../controllers/record.controllers.js";
+import { newRecord } from "../controllers/record.controllers.js";
 
 const router = Router()
 
-router.get("/records", getRecords )
-router.post("/records/:id", getRecord)
+router.post("/newRecord", newRecord);
+router.get("/", (req, res) => {
+    res.send("Hola desde API demo");
+});
 
 export default router;
